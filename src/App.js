1afeb071ec323abda2125e6edb300959dbf80269
kaushalendra verma <kaushalendra.v@celestialsys.com>
2023-10-12 17:52:29 +0530

    sample commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import NavBar from './components/NavBar.js'
+import Home from './components/Home.js'
+import About from './components/About.js'
+import Categories from './components/Categories.js'
+import Blog from './components/Blog.js'
+import Contact from './components/Contact.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
